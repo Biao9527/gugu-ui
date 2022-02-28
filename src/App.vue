46bbfd2cde8b63c73f32cd34c91bb232ac219605
周字彪 <1247821135@qq.com>
2022-02-28 16:36:06 +0800

    完成 aside 切换功能

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <router-view/>
+</template>
+
+<script>
+
+import {provide, ref} from 'vue';
+
+export default {
+  name: 'App',
+  setup(){
+    const asideVisible = ref(false)
+    provide('asideVisible',asideVisible)
+  }
+}
+</script>
