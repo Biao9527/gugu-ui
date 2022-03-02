6d9f1ffbc9f3ead1e1b394b0f3b5c00ece808605
周字彪 <1247821135@qq.com>
2022-03-02 15:19:21 +0800

    完成 button size:小、正常、大

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -1,9 +1,24 @@
 <template>
-  <Switch/>
+  <Switch v-model:value="bool" size="small"/>
+  <br>
+  <Switch v-model:value="boll2"/>
+  <br>
+  <Switch v-model:value="boll3" size="big"/>
+  <br>
+  <Switch v-model:value="boll4" :disabled="true"/>
 </template>
-<script>
+
+<script lang="ts">
 import Switch from '../lib/Switch.vue';
+import {ref} from 'vue';
 export default {
-  components: {Switch}
+  components: {Switch},
+  setup(){
+    const bool = ref<boolean>(false)
+    const boll2 = ref<boolean>(false)
+    const boll3 = ref<boolean>(false)
+    const boll4 = ref<boolean>(true)
+    return {bool,boll2,boll3,boll4}
+  }
 };
 </script>
--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -1,46 +1,122 @@
 <template>
   <div>
-    <button :class="{checked:x}" @click="toggle"><span></span></button>
+    <button class="switch"
+            :class="className"
+            @click="toggle"
+            :disabled="disabled">
+      <span></span>
+    </button>
   </div>
 </template>
 <script lang="ts">
-import {ref} from 'vue';
+
+import {computed} from 'vue';
 
 export default {
-  setup(){
-    const x = ref(false)
-    const toggle = ()=>{
-      x.value = !x.value
-    }
-    return {x,toggle}
+  props: {
+    value: Boolean,
+    size: String,
+    disabled: Boolean
+  },
+  setup(props, context) {
+    const toggle = () => {
+      context.emit('update:value', !props.value);
+    };
+    const className = computed(() => {
+      const {value, size,disabled} = props;
+      return {
+        checked: value,
+        [`switch-${size}`]: size,
+        ['switch-disabled']:disabled
+      };
+    });
+    return {toggle, className};
   }
-}
+};
 </script>
 <style lang="scss" scoped>
 $h: 22px;
 $h2: $h - 4px;
-button{
+button {
   height: $h;
-  width: $h*2;
+  width: $h * 2;
   border: none;
-  background: gray;
+  background: #bfbfbf;
   border-radius: $h/2;
   position: relative;
+  > span {
+    position: absolute;
+    top: 2px;
+    left: 2px;
+    height: $h2;
+    width: $h2;
+    background: white;
+    border-radius: $h2 / 2;
+    transition: all 250ms;
+  }
+  &.checked {
+    background: #1890ff;
+    > span {
+      left: calc(100% - #{$h2} - 2px);
+    }
+  }
+  &:focus {
+    outline: none;
+  }
+  &:active {
+    > span {
+      width: $h2 + 4px;
+    }
+  }
+  &.checked:active {
+    > span {
+      width: $h2 + 4px;
+      margin-left: -4px;
+    }
+  }
+&.switch-small {
+  width: $h * 1.2;
+  height: $h * 0.8;
+
+  > span {
+    width: $h2 * 0.75;
+    height: $h2 * 0.75;
+  }
+
+  &.checked > span {
+    left: calc(100% - #{$h2 * 0.75} - 2px);
+  }
+
+  &:active {
+    > span {
+      width: $h2;
+    }
+  }
 }
-span{
-  position: absolute;
-  top: 2px;
-  left: 2px;
-  height: $h2;
-  width: $h2;
-  background:#ffffff;
-  border-radius: $h2 / 2;
-  transition: left 250ms;
+&.switch-big {
+  height: $h * 1.4;
+  width: $h * 2.8;
+  border-radius: $h/1.4;
+  > span {
+    width: $h2 * 1.5;
+    height: $h2 * 1.5;
+    border-radius: 50%;
+  }
+  &.checked > span {
+    left: calc(100% - #{$h2 * 1.5} - 2px);
+  }
+  &:active {
+    > span {
+      width: $h2;
+    }
+  }
 }
-.checked{
-  background: blue;
+  &.switch-disabled{
+    background: #bfbfbf;
+    &:hover{
+      cursor:no-drop;
+    }
+  }
 }
-button.checked >span{
-  left: calc(100% - #{$h2} - 2px);
-}
+
 </style>
