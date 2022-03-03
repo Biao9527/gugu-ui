dc2486f9ab65968962dbf4a327c371d8b5c0a23a
周字彪 <1247821135@qq.com>
2022-03-03 15:19:42 +0800

    完成 visible 属性

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,5 +1,20 @@
 <template>
-  <div>
-    DialogDemo
-  </div>
+  <h2>示例一</h2>
+  <Button @click="toggle">toggle</Button>
+  <Dialog v-model:visible="visible"/>
 </template>
+<script>
+import Dialog from '../lib/Dialog.vue';
+import Button from '../lib/Button.vue';
+import {ref} from 'vue';
+export default {
+  components: {Button, Dialog},
+  setup(){
+    const visible = ref(false)
+    const toggle = ()=>{
+      visible.value = !visible.value
+    }
+    return {visible,toggle}
+  }
+};
+</script>
---
+++ src/lib/Dialog.vue
@@ -0,0 +1,104 @@
+<template>
+  <template v-if="visible">
+    <div class="gugu-dialog-overlay"></div>
+    <div class="gugu-dialog-wrapper">
+      <div class="gugu-dialog">
+        <header>标题<span class="gugu-dialog-close"></span></header>
+        <main>
+          <p>第一行字</p>
+          <p>第二行字</p>
+          <p>第二行字</p>
+        </main>
+        <footer>
+          <Button level="main">OK</Button>
+          <Button>Cancel</Button>
+        </footer>
+      </div>
+    </div>
+  </template>
+</template>
+<script lang="ts">
+import Button from './Button.vue';
+
+export default {
+  components: {Button},
+  props: {
+    visible: {
+      type: Boolean,
+      default: false
+    }
+  }
+};
+</script>
+<style lang="scss">
+$radius: 4px;
+$border-color: #eaecef;
+.gugu-dialog {
+  background: #ffffff;
+  border-radius: $radius;
+  box-shadow: 0 0 3px fade_out(black, 0.5);
+  min-width: 450px;
+  max-width: 90%;
+  @media (max-width: 500px) {
+    min-width: 320px;
+    max-width: 90%;
+  }
+  &-overlay {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: fade_out(black, 0.5);
+    z-index: 10;
+  }
+  &-wrapper {
+    position: fixed;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    z-index: 11;
+  }
+  > header {
+    padding: 12px 16px;
+    border-bottom: 1px solid $border-color;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    font-size: 16px;
+    font-weight: bold
+  }
+  > main {
+    padding: 24px 16px;
+    font-size: 14px;
+  }
+  > footer {
+    border-top: 1px solid $border-color;
+    padding: 12px 16px;
+    text-align: right;
+  }
+  &-close {
+    position: relative;
+    display: inline-block;
+    width: 16px;
+    height: 16px;
+    cursor: pointer;
+    &::before,
+    &::after {
+      content: '';
+      position: absolute;
+      height: 1px;
+      background: black;
+      width: 100%;
+      top: 50%;
+      left: 50%;
+    }
+    &::before {
+      transform: translate(-50%, -50%) rotate(-45deg);
+    }
+    &::after {
+      transform: translate(-50%, -50%) rotate(45deg);
+    }
+  }
+}
+</style>
