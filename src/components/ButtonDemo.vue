14e60b3c49cc6afe0fe87fb04c6e75c26b18c365
周字彪 <1247821135@qq.com>
2022-03-03 12:41:03 +0800

    完成 size属性

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -1,8 +1,21 @@
 <template>
+  <h2>示例一</h2>
   <Button>你好</Button>
   <Button theme="button">你好</Button>
-  <Button theme="link">你好</Button>
-  <Button theme="text">你好</Button>
+  <Button theme="link">Link</Button>
+  <Button theme="text">Text</Button>
+  <h2>示例二</h2>
+  <Button theme="button" size="big">大大大</Button>
+  <Button theme="button">普通通</Button>
+  <Button theme="button" size="small">小小小</Button>
+  <br>
+  <Button theme="link" size="big">大大大</Button>
+  <Button theme="link">普通通</Button>
+  <Button theme="link" size="small">小小小</Button>
+  <br>
+  <Button theme="text" size="big">大大大</Button>
+  <Button theme="text">普通通</Button>
+  <Button theme="text" size="small">小小小</Button>
 </template>
 <script>
 import Button from '../lib/Button.vue';
--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,16 +1,32 @@
 <template>
-  <button class="gugu-button" :class="`theme-${theme}`">
+  <button class="gugu-button" :class="className">
     <slot/>
   </button>
 </template>
 
 <script lang="ts">
+import {computed} from 'vue';
+
 export default {
   props: {
     theme: {
       type: String,
       default: 'button'
+    },
+    size:{
+      type:String,
+      default: 'normal'
     }
+  },
+  setup(props){
+    const className = computed(()=>{
+      const {theme,size} = props
+      return {
+        [`gugu-theme-${theme}`]:theme,
+        [`gugu-size-${size}`]:size
+      }
+    })
+    return {className}
   }
 };
 </script>
@@ -48,5 +64,32 @@ $radius: 4px;
   &::-moz-focus-inner {
     border: 0;
   }
+  &.gugu-theme-link{
+    border-color: transparent;
+    box-shadow: none;
+    color: $blue;
+    &:hover,&:focus{
+      color: lighten($blue, 10%);
+    }
+  }
+  &.gugu-theme-text{
+    border-color: transparent;
+    box-shadow: none;
+    color: inherit;
+    &:hover,&:focus{
+      background: darken(white, 5%);;
+    }
+  }
+  &.gugu-size-big {
+    font-size: 24px;
+    height: 48px;
+    padding: 0 16px
+  }
+  &.gugu-size-small {
+    font-size: 12px;
+    height: 20px;
+    padding: 0 4px;
+  }
+
 }
 </style>
