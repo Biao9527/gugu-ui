d8fd8f6e8e6a4f2e2edf611e61631da6227fc2cd
周字彪 <1247821135@qq.com>
2022-03-10 13:32:35 +0800

    创建Demo.vue 组件，并高亮代码

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -1,89 +1,28 @@
 <template>
   <div class="switch-wrapper">
     <h1>Switch 开关</h1>
-    <div class="demo">
-      <h2>基本</h2>
-      <div class="demo-component">
-        <Switch1Demo/>
-      </div>
-      <div class="demo-actions">
-        <Button>查看代码</Button>
-      </div>
-      <div class="demo-code">
-        <pre>{{Switch1Demo.__sourceCode}}</pre>
-      </div>
-    </div>
-    <div class="demo">
-      <h2>尺寸</h2>
-      <div class="demo-component">
-        <Switch2Demo/>
-      </div>
-      <div class="demo-actions">
-        <Button>查看代码</Button>
-      </div>
-      <div class="demo-code">
-        <pre>{{Switch2Demo.__sourceCode}}</pre>
-      </div>
-    </div>
-    <div class="demo">
-      <h2>不可用状态</h2>
-      <div class="demo-component">
-        <Switch3Demo/>
-      </div>
-      <div class="demo-actions">
-        <Button>查看代码</Button>
-      </div>
-      <div class="demo-code">
-        <pre>{{Switch3Demo.__sourceCode}}</pre>
-      </div>
-    </div>
+    <Demo :component="Switch1Demo"/>
+    <Demo :component="Switch2Demo"/>
+    <Demo :component="Switch3Demo"/>
   </div>
 </template>
 
 <script lang="ts">
-import Button from '../lib/Button.vue';
 import Switch1Demo from './Switch1.demo.vue';
 import Switch2Demo from './Switch2.demo.vue';
 import Switch3Demo from './Switch3.demo.vue';
+import Demo from './Demo.vue';
 
 export default {
-  components: {Switch3Demo, Switch2Demo, Switch1Demo, Button},
+  components: {Demo, Switch3Demo, Switch2Demo, Switch1Demo},
   setup() {
-    return{
-      Switch1Demo,Switch2Demo,Switch3Demo
-    }
+    return {Switch1Demo, Switch2Demo, Switch3Demo};
   }
 };
 </script>
 
 <style lang="scss">
-$border-color: #d9d9d9;
 .switch-wrapper {
   margin: 0 20px;
-  .demo {
-    border: 1px solid $border-color;
-    margin: 16px 0 32px;
-    > h2 {
-      font-size: 20px;
-      padding: 8px 16px;
-      border-bottom: 1px solid $border-color;
-    }
-    &-component {
-      padding: 16px;
-    }
-    &-actions {
-      padding: 8px 16px;
-      border-top: 1px dashed $border-color;
-    }
-    &-code {
-      padding: 8px 16px;
-      border-top: 1px dashed $border-color;
-      > pre {
-        line-height: 1.1;
-        font-family: Consolas, 'Courier New', Courier, monospace;
-        margin: 0;
-      }
-    }
-  }
 }
 </style>
